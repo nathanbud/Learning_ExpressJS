c2805d3ef3971438b1e0c72626bb349158ac933d
nathanbud <nthnlbdky916@gmail.com>
2019-10-13 23:10:07 +0800

    added not found route

--- hello-world/index.js
+++ hello-world/index.js
@@ -4,13 +4,13 @@ var app = express();
 var things = require('./things.js');
 
 
-// app.get('/hello', function(req, res){
-//     res.send("Hello World");
-// });
+app.get('/hello', function(req, res){
+    res.send("Hello World");
+});
 
-// app.post('/hello', function(req, res){
-//     res.send("You just called the post method at '/hello' !\n ");
-// });
+app.post('/hello', function(req, res){
+    res.send("You just called the post method at '/hello' !\n ");
+});
 
 app.use('/things', things);
 
